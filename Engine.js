edb6113936de4abc2fa779fe874bab6d79cce677
thcheetah777 <thcheetah777@gmail.com>
2022-02-16 15:35:46 -0800

    Add game proportions

--- Engine.js
+++ Engine.js
@@ -7,6 +7,8 @@ class Engine {
   constructor(phaser) {
     this.phaser = phaser;
     this.mouseDown = false;
+    this.gameWidth = this.sys.game.canvas.width;
+    this.gameHeight = this.sys.game.canvas.height;
   }
 
   // ---------- Tools ----------
